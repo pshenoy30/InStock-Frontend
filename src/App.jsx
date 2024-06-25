58ec5355dd70982acaaa91a2a092c02f9594a614
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-25 15:06:30 +0400

    Added the setup for the browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "../pages/Homepage/Homepage.jsx";
+import Wearhouse from "../pages/Wearhouse/Wearhouse.jsx";
+import Inventory from "../pages/Inventory/Inventory.jsx";
+import InventoryItem from "../pages/InventoryItem/InventoryItem.jsx";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage.jsx"
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/wearhouse/:wearhouseId" element={<Wearhouse />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/inventory/:inventoryId" element={<InventoryItem />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
