190afe00e129d5da9be524137a81280eee2d255d
rouenemedina <rouenemedina@gmail.com>
2024-06-25 10:48:49 -0400

    created the footer component and the styles for all media queries

--- src/pages/warehouses/warehouses.jsx
+++ src/pages/warehouses/warehouses.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function Warehouses() {
     return (
         <>
           <Header />  
+          <Footer />
         </>
     );
 }
