63726b801d0907194858dad7ef5cc26af3c58250
rouenemedina <rouenemedina@gmail.com>
2024-06-26 21:36:56 -0400

    styled the list component for mobile and tablet media queries

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,58 +1,73 @@
-import { useState } from 'react';
-import MediaQuery from 'react-responsive';
+import MediaQuery from "react-responsive";
 import deleteImg from "../../assets/icons/delete_outline-24px.svg";
 import editImg from "../../assets/icons/edit-24px.svg";
+import arrowImg from "../../assets/icons/chevron_right-24px.svg";
 import "./List.scss";
-function List({id, title1, val1, title2, val2, title3, val3, title4, val4, title5, val5}) {
-    
-    return (
+
+function List({
+  id,
+  title1,
+  val1,
+  title2,
+  val2,
+  title3,
+  val3,
+  title4,
+  val4,
+  title5,
+  val5,
+}) {
+  return (
     <>
-        <MediaQuery maxWidth={767}>
-            <article className="list" key={id}>
-            <div className="divider"></div>
-            <article className="list__left">
-                <h3 className="list__title" >{title1}</h3>
-                <h3 className="list__detail  list__detail--blue">{val1}</h3>
-                <h3 className="list__title" >{title2}</h3>
-                <h3 className="list__detail  list__detail--blue list__detail__address">{val2}</h3>
-            </article>
-            <article className="list__right">
-                <h3 className="list__title" >{title3}</h3>
-                <h3 className="list__detail  list__detail--blue">{val3}</h3>
-                <h3 className="list__title" >{title4}</h3>
-                <h3 className="list__detail  list__detail--blue list__detail__address">{val4}</h3>
-            </article>
-            </article>
-        </MediaQuery>
-        <MediaQuery minWidth={768}>
-            {/* <article className="list"> */}
-                <table className="list__table">
-                {/* <thead className="list__header">
-                    <tr className="list__row">
-                    <th className="list__columnheader">{title1}</th>
-                    <th className="list__columnheader">{title2}</th>
-                    <th className="list__columnheader">{title3}</th>
-                    <th className="list__columnheader">{title4}</th>
-                    <th className="list__columnheader">ACTIONS</th>
-                    </tr>
-                </thead> */}
-                <tbody className="list__body">
-                    <tr className="list__records" key={id}>
-                    <td className="list__data">{val1}</td>
-                    <td className="list__data">{val2}</td>
-                    <td className="list__data">{val3}</td>
-                    <td className="list__data">{val4}</td>
-                    <td className="list__actions">
-                        <img src={deleteImg} alt="delete button"></img>
-                        <img src={editImg} alt="edit button"></img>
-                    </td>
-                    </tr>
-                </tbody>
-                </table>
-            {/* </article> */}
-        </MediaQuery>
-      </>
-  )
+      <MediaQuery maxWidth={767}>
+        <article className="list" key={id}>
+          <article className="list__left">
+            <div className="list__card">
+              <h3 className="list__title">{title1}</h3>
+              <div className="list__subcard">
+                <h3 className="list__entry list__entry--interactive">{val1}</h3>
+                <img className="list__img" src={arrowImg} alt="expand details button"></img>
+              </div>
+            </div>
+            <div className="list__card">
+              <h3 className="list__title">{title2}</h3>
+              <h3 className="list__detail">{val2}</h3>
+            </div>
+            <img className="list__img" src={deleteImg} alt="delete button"></img>
+          </article>
+          <article className="list__right">
+            <div className="list__card">
+              <h3 className="list__title">{title3}</h3>
+              <h3 className="list__detail">{val3}</h3>
+            </div>
+            <div className="list__card">
+              <h3 className="list__title">{title4}</h3>
+              <h3 className="list__detail">
+                {val4}
+              </h3>
+            </div>
+            <img className="list__img list__img--right" src={editImg} alt="edit button"></img>
+          </article>
+        </article>
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <table className="list__table">
+          <tbody className="list__body">
+            <tr className="list__records" key={id}>
+              <td className="list__detail list__detail--left">{val1} <img className="list__img list__img__arrow" src={arrowImg} alt="expand details button"></img></td>
+              <td className="list__detail list__detail--smaller">{val2}</td>
+              <td className="list__detail">{val3}</td>
+              <td className="list__detail list__detail--larger">{val4}</td>
+              <td className="list__actions">
+                <img src={deleteImg} alt="delete button"></img>
+                <img src={editImg} alt="edit button"></img>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </MediaQuery>
+    </>
+  );
 }
 
 export default List;
