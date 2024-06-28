f6fd668af605038c009c8a39e4cd043b7d600b41
rouenemedina <rouenemedina@gmail.com>
2024-06-28 11:17:07 -0400

    fixing the desktop media query for the inventory item

--- src/components/EditNav/EditNav.jsx
+++ src/components/EditNav/EditNav.jsx
@@ -2,7 +2,12 @@ import Buttons from "../Buttons/Buttons";
 import backImg from "../../assets/icons/arrow_back-24px.svg"
 import "./EditNav.scss";
 
-function EditNav({title, buttonText}) {
+//TODO: Button Text not showing up
+function EditNav({title, buttonText, hideButton}) {
+    let hideButtonClass = "editNav__btn";
+    if (hideButton) {
+        hideButtonClass += " editNav__btn--hidden";
+    }
     return (
       <>
         <section className="editNav">
@@ -10,7 +15,7 @@ function EditNav({title, buttonText}) {
             <img src={backImg} alt="back button" className="editNav__img"/>
             <h1 className="editNav__title">{title}</h1>
           </div>
-          <Buttons buttonName={buttonText} />
+          <Buttons buttonName={buttonText} className={hideButtonClass}/>
         </section>
       </>
     );
