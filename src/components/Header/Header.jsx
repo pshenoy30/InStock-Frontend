97c66b9ee98a154c4e32a9dd517cb99d257c8732
rouenemedina <rouenemedina@gmail.com>
2024-06-27 22:11:43 -0400

    updated styling for header to show up active state when clicked

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import "./Header.scss";
 import InStockLogo from "../../assets/logo/InStock-Logo_1x.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
@@ -9,10 +9,10 @@ function Header() {
         <img className="header__img" src={InStockLogo} alt="InStock Logo"></img>
       </section>
       <nav className="header__nav">
-        <NavLink to="/">
+        <NavLink to="/" className="header__link">
           <h3 className="header__navItem">Warehouses</h3>
         </NavLink>
-        <NavLink to="/inventories">
+        <NavLink to="/inventories" className="header__link">
           <h3 className="header__navItem">Inventory</h3>
         </NavLink>
       </nav>
